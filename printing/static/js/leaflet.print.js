a7fedb0bbf03eedf3373d19b326bf69452876482
Rocky Chen <Rocky.Chen@dpaw.wa.gov.au>
2016-05-03 08:54:08 +0800

    Fix the wrong boundingbox coordinate value printed on the map.

--- printing/static/js/leaflet.print.js
+++ printing/static/js/leaflet.print.js
@@ -1,9 +1,9 @@
 "use strict";
 L.Print = function(mapid,login_user,data) {
-    $("#map_north_west").html("(" + data.bounds[0] + "," + data.bounds[1]+ ")");
-    $("#map_north_east").html("(" + data.bounds[0] + "," + data.bounds[3]+ ")");
-    $("#map_south_west").html("(" + data.bounds[2] + "," + data.bounds[1]+ ")");
-    $("#map_south_east").html("(" + data.bounds[2] + "," + data.bounds[3]+ ")");
+    $("#map_north_west").html("(" + data.bounds[2] + "," + data.bounds[1]+ ")");
+    $("#map_north_east").html("(" + data.bounds[2] + "," + data.bounds[3]+ ")");
+    $("#map_south_west").html("(" + data.bounds[0] + "," + data.bounds[1]+ ")");
+    $("#map_south_east").html("(" + data.bounds[0] + "," + data.bounds[3]+ ")");
 
     $("#map_zoom_level").html(data.zoom);
     $("#map_creator").html(login_user.email);
